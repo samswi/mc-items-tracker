5a51765c6a85cc285d4f1f995e24d7a3ca1ea1b1
samswi <samswi965@gmail.com>
2025-12-03 17:29:31 +0100

    1.0-1.21.10

--- src/client/java/com/samswi/itemsTracker/client/RemainingItemsScreen.java
+++ src/client/java/com/samswi/itemsTracker/client/RemainingItemsScreen.java
@@ -1,11 +1,11 @@
 package com.samswi.itemsTracker.client;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.TextWidget;
-import net.minecraft.client.gui.widget.Widget;
+import net.minecraft.client.gui.widget.*;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -14,56 +14,88 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.HoverEvent;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class RemainingItemsScreen extends Screen {
     TextWidget myTextWidget;
+    private ScrollableLayoutWidget scrollableLayoutWidget;
+    GridWidget grid;
+    GridWidget.Adder gridAdder;
+    final MinecraftClient client = MinecraftClient.getInstance();
+    public final ThreePartsLayoutWidget layout = new ThreePartsLayoutWidget(this);
+
 
     RemainingItemsScreen(){
         super(Text.of("Remaining Items"));
 
         myTextWidget = new TextWidget(Text.of("Remaining items"), MinecraftClient.getInstance().textRenderer);
 
+        layout.addHeader(Text.of("Item Tracker"), client.textRenderer);
+        this.layout.addFooter(ButtonWidget.builder(ScreenTexts.DONE, (button) -> {
+            this.close();
+        }).width(200).build());
+        grid = new GridWidget();
+        grid.getMainPositioner()
+                .margin(4);
+        gridAdder = grid.createAdder(12);
+
+        for (String i : ItemsTrackerClient.goalItems){
+            BackgroundedItemStackWidget itemWidget;
+            if (ItemsTrackerClient.remainingItems.contains(i)){
+                itemWidget = new BackgroundedItemStackWidget(client, 0, 0, 16, 16, Text.of(""), new ItemStack(Registries.ITEM.get(Identifier.of(i))), true, true, 0x88000000);
+            } else {
+                itemWidget = new BackgroundedItemStackWidget(client, 0, 0, 16, 16, Text.of(""), new ItemStack(Registries.ITEM.get(Identifier.of(i))), true, true, 0xFF00FF00);
+            }
+            gridAdder.add(itemWidget);
+        }
+
+        scrollableLayoutWidget = new ScrollableLayoutWidget(client, grid, layout.getContentHeight());
+
+        layout.addBody(scrollableLayoutWidget);
+
     }
-
-
 
     @Override
     protected void init() {
-        ItemsTrackerClient.remainingItems.forEach(s -> {
-            ItemWidget myItemWidget = new ItemWidget(s);
-            this.addDrawable(myItemWidget);
-        });
-        myTextWidget.setPosition((MinecraftClient.getInstance().getWindow().getScaledWidth()/2)- myTextWidget.getWidth()/2, 10);
-        addDrawableChild(myTextWidget);
         super.init();
+
+        layout.setPosition(0, 0);
+
+        layout.refreshPositions();
+        scrollableLayoutWidget.setHeight(layout.getContentHeight());
+        scrollableLayoutWidget.refreshPositions();
+        scrollableLayoutWidget.setPosition(scrollableLayoutWidget.getX(), layout.getHeaderHeight());
+        grid.refreshPositions();
+        layout.forEachChild(this::addDrawableChild);
+
     }
 
-    public class ItemWidget implements Drawable {
-        Identifier itemId;
-        int x;
-        int y;
-        public ItemStack myItemStack;
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float deltaTicks) {
+        context.drawTexture(RenderPipelines.GUI_TEXTURED, Screen.FOOTER_SEPARATOR_TEXTURE, 0, this.height - this.layout.getFooterHeight(), 0.0F, 0.0F, this.width, 2, 32, 2);
+        context.drawTexture(RenderPipelines.GUI_TEXTURED, Screen.HEADER_SEPARATOR_TEXTURE, 0, this.layout.getHeaderHeight() - 2, 0.0F, 0.0F, this.width, 2, 32, 2);
+        this.renderDarkening(context, 0, this.layout.getHeaderHeight(), this.width, layout.getContentHeight());
+        super.render(context, mouseX, mouseY, deltaTicks);
+    }
 
-        public ItemWidget(String itemIdString) {
-            this.itemId = Identifier.of(itemIdString);
-            x = (int) (Math.random()* (MinecraftClient.getInstance().getWindow().getScaledWidth()-16));
-            y = (int) (Math.random()* (MinecraftClient.getInstance().getWindow().getScaledHeight()-16));
+    public static class BackgroundedItemStackWidget extends ItemStackWidget{
+        int backgroundColor;
 
-            myItemStack = new ItemStack(Registries.ITEM.get(itemId));
+        public BackgroundedItemStackWidget(MinecraftClient client, int x, int y, int width, int height, Text message, ItemStack stack, boolean drawOverlay, boolean hasTooltip, int background) {
+            super(client, x, y, width, height, message, stack, drawOverlay, hasTooltip);
+            backgroundColor = background;
 
         }
 
-        @Override
-        public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-            MinecraftClient client = MinecraftClient.getInstance();
-            context.drawItem(myItemStack, x, y);
-            if (mouseX >= x && mouseX < x + 16 && mouseY >= y && mouseY < y + 16) {
-                context.drawTooltip(MinecraftClient.getInstance().textRenderer, myItemStack.getTooltip(Item.TooltipContext.DEFAULT, client.player, client.options.advancedItemTooltips ? TooltipType.Default.ADVANCED : TooltipType.Default.BASIC), mouseX, mouseY);
-            }
 
+        @Override
+        protected void renderWidget(DrawContext context, int mouseX, int mouseY, float deltaTicks) {
+            context.fill(this.getX()-4, this.getY()-4, this.getX()+this.getWidth()+4, this.getY()+this.getHeight()+4, backgroundColor);
+            super.renderWidget(context, mouseX, mouseY, deltaTicks);
         }
     }
 }
+
